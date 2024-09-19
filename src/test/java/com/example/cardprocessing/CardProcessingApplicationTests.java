87e276d1b2c55012e97e693314c69073bfbd3db6
Hasan Baxriddinov <h.baxriddinov@agrobank.uz>
2024-09-19 14:56:07 +0500

    first commit

---
+++ src/test/java/com/example/cardprocessing/CardProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cardprocessing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardProcessingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
